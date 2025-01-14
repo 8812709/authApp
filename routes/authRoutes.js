ec91d3c5724c0ee47773e7f9bb3b221eb682c670
rakib hussain <rakibhussain8812@gmail.com>
2025-01-15 01:36:52 +0530

    get data adde

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const router=express.Router()
 // defning route handlers 
 const{signupHandler,loginHandler}=require("../controllers/auth")
 const{auth,isAdmin,isStudent}=require("../middlewares/middleware")
+const User=require("../models/userSchema")
 
 //linking the handlers w routes
 router.post("/login",loginHandler)
@@ -28,6 +29,24 @@ router.get("/student",auth,isStudent,(req,res)=>{
     res.status(200).json({
         success:true,
         message:"Welcome to the protected Route of Student"
+    })
+})
+
+router.get("/getData",auth,async(req,res)=>{
+    const id=req.user._id
+    const user=await User.findOne({id})
+    user.password=undefined
+    if(!user)
+    {
+        return res.status(404).json({
+            success:false,
+            message:"no data found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        data:user,
+        message:"The data is found"
     })
 })
 
