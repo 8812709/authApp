7ef1e89bf66a0f5dfa690eba1962fcedb30b0706
rakib hussain <rakibhussain8812@gmail.com>
2025-01-14 18:18:38 +0530

    added middleware

---
+++ middlewares/middleware.js
@@ -0,0 +1,70 @@
+//function which will check the authenticity for the roles of admin and student
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
+
+
+exports.auth=(req,res,next)=>{
+    //parse the value of token 
+    const {token}=req.body
+
+    //check if the token is present or not
+    if(!token)
+    {
+        return res.status(404).json({
+            success:false,
+            message:"Token Missing"
+        })
+    }
+
+    //if token is found then verify it 
+    try {
+        const payload=jwt.verify(token,process.env.JWT_SECRET)
+        console.log(payload)
+        req.user=payload //inserting the value of payload in request
+    } catch (error) {
+        return res.status(403).json({
+            success:false,
+            message:"Invalid Token"
+        })
+        
+    }
+    next()
+}
+
+exports.isStudent=(req,res,next)=>{
+    // parse the role from the user
+    try {
+        if(req.user.role!=="Student"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a protected route for student,u cannot access it"
+            })
+        }
+        next()
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"user role is not matching"
+        })
+    }
+}
+
+//function for the admin route
+
+exports.isAdmin=(req,res,next)=>{
+    try {
+        if(req.user.role!=="Admin"){
+            return res.status(403).json({
+                success:false,
+                message:"This is a Protected route for Admin,u cannot get there"
+            })
+        }
+        next()
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"The user route is not matching"
+        })
+    }
+    
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,33 @@ const router=express.Router()
 
 // defning route handlers 
 const{signupHandler,loginHandler}=require("../controllers/auth")
+const{auth,isAdmin,isStudent}=require("../middlewares/middleware")
 
 //linking the handlers w routes
 router.post("/login",loginHandler)
 router.post("/signup",signupHandler)
+
+//protected routes using middleware 
+router.get("/test",auth,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"test successfull for auth middleware,its working fine"
+    })
+})
+
+router.get("/admin",auth,isAdmin,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Welcome to the protected Route of Admin"
+    })
+})
+
+router.get("/student",auth,isStudent,(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Welcome to the protected Route of Student"
+    })
+})
 
 
 //exporting the module
